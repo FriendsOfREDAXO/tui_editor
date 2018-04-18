fc0b741227a0b6fc0c2818686f26c291fdb94776
Joachim Dörr <mail@joachim-doerr.com>
2018-04-18 20:46:33 +0200

    add first tui profile

--- assets/tui.js
+++ assets/tui.js
@@ -1,32 +1,33 @@
 $(function () {
     tui_init();
 
-    // $(document).on('pjax:end', function () {
-    // tui_init();
-    // });
+    $(document).on('pjax:success', function () {
+        tui_init();
+    });
 });
 
 function tui_init() {
 
-    var editor = new tui.Editor({
-        el: document.querySelector('#editSection'),
-        initialEditType: 'markdown',
-        previewStyle: 'vertical',
-        height: 'auto',
-        minHeight: '200px',
-        events: {
-            change: function(){
-                $('#formEditSection').val(editor.getValue());
+    var editor1 = $('.tui-editor-default');
+
+    editor1.each(function (index) {
+        var tui_input = $(this);
+        var tui_editor = $('<div id="input_' + index + '_0"></div>');
+
+        tui_input.parent().append(tui_editor);
+
+        var editor_tui = new tui.Editor({
+            el: document.querySelector('#' + tui_editor.attr('id')),
+            initialEditType: 'wysiwyg',
+            previewStyle: 'vertical',
+            initialValue: tui_input.val(),
+            height: 'auto',
+            events: {
+                change: function () {
+                    tui_input.val(editor_tui.getValue());
+                }
             }
-        }
+        });
     });
 
-
-    // $('#REX_FORM').addEventListener('submit', function() {
-    //     $('#editSectionText').val(editor.getValue());
-    //
-    //     return false;
-    // });
-
 }
-
